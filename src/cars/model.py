999e58bc8b58708582994de1bee40235a3b35296
Starobogatov Alexey <Starobogatov.a@yandex.ru>
2024-08-05 10:59:26 +0300

    add models

---
+++ src/cars/model.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import BaseModel, str16
+
+
+class Car(BaseModel):
+    __tablename__ = "car"
+
+    brand: Mapped[str16] = mapped_column(index=True)
+    model: Mapped[str16]
+    year: Mapped[int]
+    fuel_type: Mapped[str16]  # Были созданы ENUM, но в БД используется просто str (валидация на уровне Pydantic)
+    transmission: Mapped[str16]  # что бы избежать проблем с миграциями Alembic (он не удаляет ENUM)
+    mileage: Mapped[int]
+    price: Mapped[int]
---
+++ src/models.py
@@ -0,0 +1,18 @@
+import bcrypt
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import BaseModel, str32, str16
+
+
+class User(BaseModel):
+    __tablename__ = "user"
+
+    hashed_password: Mapped[bytes | None]
+    email: Mapped[str32 | None] = mapped_column(index=True, unique=True)
+    name: Mapped[str16] = mapped_column(index=True)
+    is_admin: Mapped[bool] = mapped_column(default=False, server_default="false")
+
+    def check_password(self, plain_password: str) -> bool:
+        if self.hashed_password is None:
+            return False
+        return bcrypt.checkpw(plain_password.encode(), self.hashed_password)  # noqa
